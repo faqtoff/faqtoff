89bc3a34cc61a61136d64cdf4c3eff97e7d64ccb
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2021-07-29 21:46:16 -0300

    beta

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,17 @@ const express = require("express");
 const cors = require("cors");
 
 
+const APP_NAME = 'Faqtoff.com'
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+      user: gmailEmail,
+      pass: gmailPassword,
+  }
+});
+
 ////////////////////////////////////////////////////////////////////////////////////////////////// CONTACT.ME
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
@@ -17,16 +28,6 @@ app.post("/", (req, res) => {
     return res.status(400).send({ message: "invalid request" });
   }
 
-  const APP_NAME = 'FaqToff.com'
-  const gmailEmail = functions.config().gmail.email;
-  const gmailPassword = functions.config().gmail.password;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: gmailEmail,
-        pass: gmailPassword,
-    }
-  });
 
   const mailOptions = {
     from:`${APP_NAME} <noreply@faqtoff.com>`,
@@ -47,16 +48,6 @@ module.exports.mailer = functions.https.onRequest(app);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////// AUTH
 
-const APP_NAME = 'FaqToff.com'
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-      user: gmailEmail,
-      pass: gmailPassword,
-  }
-});
 exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
   const email = user.email;
   const displayName = user.displayName;
@@ -73,8 +64,24 @@ async function sendWelcomeEmail(email, displayName) {
     to: email,
   };
   mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-  await mailTransport.sendMail(mailOptions);
+  mailOptions.text = `
+  <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="x-apple-disable-message-reformatting">
+        <meta content="telephone=no" name="format-detection">
+        <title></title>
+      </head>
+    <body>
+        <h1>Welcome to Faqtoff.com!</h1>
+        <h3> Hey ${displayName || ''}! </h3>
+        <p>Welcome to ${APP_NAME}. I hope you will enjoy our service.</p>
+    </body>
+  </html>`;
+  await transporter.sendMail(mailOptions);
   functions.logger.log('New welcome email sent to:', email);
   return null;
 }
@@ -85,7 +92,7 @@ async function sendGoodbyeEmail(email, displayName) {
   };
   mailOptions.subject = `Bye!`;
   mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
-  await mailTransport.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
   functions.logger.log('Account deletion confirmation email sent to:', email);
   return null;
 }
